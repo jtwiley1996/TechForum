f9d9a888f18939905d57bd112585937a90d3b1c6
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-06-14 15:25:53 -0500

    added changes to .env and mysql

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
+    // Connect to Heroku MySQL database
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+    // Connect to local MySQL database
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
@@ -16,15 +18,16 @@ if (process.env.JAWSDB_URL){
             port: 3306
         }
     );
-};
+}
 
+// Test the connection
 sequelize.authenticate()
     .then(() => {
         console.log('Connection has been established successfully.');
-        console.log(`${process.env.DB_NAME} database connected`)
+        console.log(`${process.env.DB_NAME} database connected`);
     })
     .catch(err => {
-        console.error('Unable to connect to the database:', err)
+        console.error('Unable to connect to the database:', err);
     });
 
 module.exports = sequelize;
